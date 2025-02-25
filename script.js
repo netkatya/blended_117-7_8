5e91f065e02ea74bdc6d11eca5b3474d2698f3e1
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-02-25 22:40:38 +0000

    modified files

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".question-button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const answer = this.closest("li").querySelector(".question-item-answer");
+
+      // Toggle visibility
+      answer.style.display = answer.style.display === "none" || answer.style.display === "" ? "block" : "none";
+
+      // Change the icon (optional)
+      const icon = this.querySelector("use");
+      if (icon.getAttribute("href") === "./images/icons.svg#icon-minus_circle") {
+        icon.setAttribute("href", "./images/icons.svg#icon-plus_circle");
+      } else {
+        icon.setAttribute("href", "./images/icons.svg#icon-minus_circle");
+      }
+    });
+  });
+});
+
